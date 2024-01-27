32f7f358225eddab540699c164a6332c526f5fc2
Tincho <tinroldandg@gmail.com>
2024-01-27 14:56:02 -0500

    throw hen

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //[SerializeField]
+    //private GameObject r_weaponPrefab;
+    [SerializeField]
+    private Transform handPlace;
+
+    [SerializeField]
+    private PlayerWeapon myWeapon;
+
+    [SerializeField]
+    private float throwForce=300f;
+
+    [SerializeField]
+    private Transform directionObject;
+
+    private float distansWeapon;
+    private bool withMe;
+
+    private void Awake()
+    {
+        
+        withMe = true;
+        //r_myWeapon = Instantiate(r_weaponPrefab, r_handPlace.transform.position, Quaternion.identity).GetComponent<PlayerWeapon>();
+        myWeapon.transform.SetParent(handPlace);
+        myWeapon.transform.localRotation = Quaternion.Euler(Vector3.zero);
+        myWeapon.transform.localPosition = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftControl)&&withMe==true)
+        {
+            Throw();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PickUp();
+        }
+    }
+
+    public void Throw()
+    {
+        myWeapon.transform.SetParent(null);
+        myWeapon.Throw(throwForce, directionObject.forward);
+        withMe = false;
+    }
+
+    public void PickUp()
+    {
+        myWeapon.PickUp();
+        myWeapon.transform.SetParent(handPlace);
+        myWeapon.transform.localRotation = Quaternion.Euler(Vector3.zero);
+        myWeapon.transform.localPosition = Vector3.zero;
+        withMe = true;
+    }
+
+}
---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+
+    private Rigidbody myRB;
+
+    private void Awake()
+    {
+        myRB = GetComponent<Rigidbody>();
+        myRB.useGravity = false;
+        myRB.isKinematic = true;
+    }
+
+    public void Throw(float force,Vector3 direction)
+    {
+        myRB.useGravity = true;
+        myRB.isKinematic = false;
+        myRB.velocity = direction * force * Time.deltaTime;
+    }
+
+    public void PickUp()
+    {
+        myRB.useGravity = false;
+        myRB.isKinematic = true;
+
+
+    }
+}
